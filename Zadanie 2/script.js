fc6db55639cf8de7ef7e62c0b473fe12d5ba24d6
Piotr Tomczak <230025@edu.p.lodz.pl>
2021-10-10 09:50:48 +0200

    Page is working

--- Zadanie 2/script.js
+++ Zadanie 2/script.js
@@ -1,5 +1,5 @@
 "use strict";
-let todoList = []; //declares a new array for Your todo list
+let todoList = [];
 let initList = function () {
   todoList.push(
     {
@@ -14,20 +14,14 @@ let initList = function () {
       place: "F6",
       dueDate: new Date(2019, 10, 17),
     }
-    // of course the lecture test mentioned above will not take place
   );
 };
-
 initList();
 let updateTodoList = function () {
   let todoListDiv = document.getElementById("todoListView");
-
-  //remove all elements
   while (todoListDiv.firstChild) {
     todoListDiv.removeChild(todoListDiv.firstChild);
   }
-
-  //add all elements
   for (let todo in todoList) {
     let newElement = document.createElement("div");
     let newContent = document.createTextNode(
@@ -39,41 +33,3 @@ let updateTodoList = function () {
 };
 
 setInterval(updateTodoList, 1000);
-
-let addTodo = function () {
-  //get the elements in the form
-  let inputTitle = document.getElementById("inputTitle");
-  let inputDescription = document.getElementById("inputDescription");
-  let inputPlace = document.getElementById("inputPlace");
-  let inputDate = document.getElementById("inputDate");
-  //get the values from the form
-  let newTitle = inputTitle.value;
-  let newDescription = inputDescription.value;
-  let newPlace = inputPlace.value;
-  let newDate = new Date(inputDate.value);
-  //create new item
-  let newTodo = {
-    title: newTitle,
-    description: newDescription,
-    place: newPlace,
-    dueDate: newDate,
-  };
-  //add item to the list
-  todoList.push(newTodo);
-};
-
-let newDeleteButton = document.createElement("input");
-newDeleteButton.type = "button";
-newDeleteButton.value = "x";
-newDeleteButton.addEventListener("click", function () {
-  deleteTodo(todo);
-});
-
-newElement.appendChild(newDeleteButton);
-
-let deleteTodo = function (index) {
-  todoList.splice(index, 1);
-};
-
-let savedList = window.localStorage.getItem("todos");
-if (savedList != null) todoList = JSON.parse(savedList);
