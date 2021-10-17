f7a37e6ab10fd8bbd2b135e0863fe6e71f83700c
Piotr Tomczak <230025@edu.p.lodz.pl>
2021-10-17 18:32:57 +0200

    initial for list

---
+++ Zadanie 3/app/src/components/GenreList.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h3>Lista według gatunku</h3>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "GenreList",
+});
+</script>
+
+<style scoped></style>
