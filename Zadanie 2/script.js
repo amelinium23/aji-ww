2335cb2d19de8dc94dd66a845b62c0c962a5248e
Piotr Tomczak <230025@edu.p.lodz.pl>
2021-10-17 15:56:39 +0200

    anonymus functions are better

--- Zadanie 2/script.js
+++ Zadanie 2/script.js
@@ -17,7 +17,7 @@ $.ajax({
   },
 });
 
-let updateJSONbin = function () {
+let updateJSONbin = () => {
   $.ajax({
     url: "https://api.jsonbin.io/b/6162a90aaa02be1d445747f8",
     type: "PUT",
@@ -37,7 +37,7 @@ let updateJSONbin = function () {
   });
 };
 
-let deleteTodo = function (index) {
+let deleteTodo = (index) => {
   todoList.splice(index, 1);
   updateJSONbin();
 };
@@ -56,7 +56,7 @@ let createNewTableRow = (todo, index) => {
   return rowElement;
 };
 
-let updateTodoList = function () {
+let updateTodoList = () => {
   let todoListTable = $("#todoTableBody");
   let titleValue = $("#inputSearch").val();
   let toDateValue = new Date($("#toDateSearch").val());
@@ -67,11 +67,11 @@ let updateTodoList = function () {
   for (let todo in todoList) {
     if (
       (titleValue == "" ||
-      todoList[todo].title.includes(titleValue) ||
-      todoList[todo].description.includes(titleValue)) && 
-      (isNaN(Date.parse(fromDateValue)) || 
-      new Date(todoList[todo].dueDate) >= fromDateValue) &&
-        (isNaN(Date.parse(toDateValue)) || 
+        todoList[todo].title.includes(titleValue) ||
+        todoList[todo].description.includes(titleValue)) &&
+      (isNaN(Date.parse(fromDateValue)) ||
+        new Date(todoList[todo].dueDate) >= fromDateValue) &&
+      (isNaN(Date.parse(toDateValue)) ||
         new Date(todoList[todo].dueDate) <= toDateValue)
     ) {
       const newElement = createNewTableRow(todoList[todo], todo);
@@ -81,7 +81,7 @@ let updateTodoList = function () {
 };
 setInterval(updateTodoList, 1000);
 
-let addTodo = function () {
+let addTodo = () => {
   let newTitle = $("#inputTitle").val();
   let newDescription = $("#inputDescription").val();
   let newPlace = $("#inputPlace").val();
