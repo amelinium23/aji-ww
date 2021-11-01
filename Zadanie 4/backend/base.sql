235009e63669caf98f17634d8b5a802e246c2c28
Piotr Tomczak <piotrek200011@gmail.com>
2021-11-01 11:08:12 +0100

    inserts for orders

--- Zadanie 4/backend/base.sql
+++ Zadanie 4/backend/base.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS products(id INT AUTO_INCREMENT PRIMARY KEY, name TEXT, description TEXT, price DECIMAL(4,2), weight DECIMAL(3,2), category_id INT NOT NULL,FOREIGN KEY (category_id) REFERENCES categories(id));
 CREATE TABLE IF NOT EXISTS categories(id INT AUTO_INCREMENT PRIMARY KEY, name TEXT)
-CREATE TABLE IF NOT EXISTS statuses(id INT AUTO_INCREMENT PRIMARY KEY, name TEXT NOT NULL);
-CREATE TABLE IF NOT EXISTS orders(id INT AUTO_INCREMENT PRIMARY KEY, state ENUM ('NIEZATWIERDZONE', 'ZATWIERDZONE', 'ANULOWANE', 'ZREALIZOWANE'),approval_date DATE,	username TEXT, email TEXT, product_id UNSIGNED INT,);
+CREATE TABLE IF NOT EXISTS statuses(id INT AUTO_INCREMENT PRIMARY KEY, name ENUM ('NIEZATWIERDZONE', 'ZATWIERDZONE', 'ANULOWANE', 'ZREALIZOWANE'));
+CREATE TABLE orders(id INT AUTO_INCREMENT PRIMARY KEY,state INT, approval_date DATE,username TEXT,email TEXT, product_id INT,FOREIGN KEY (state) REFERENCES statuses(id));
 
 INSERT INTO categories(name) VALUES ('fruits');
 INSERT INTO categories(name) VALUES ('vegetables');
@@ -11,3 +11,12 @@ INSERT INTO products(name, description, price, weight, category_id) VALUES('Appl
 INSERT INTO products(name, description, price, weight, category_id) VALUES('Onion', 'Polish onion make Poland great again', 0.90, 1.00, 2);
 INSERT INTO products(name, description, price, weight, category_id) VALUES('Tomato', 'Mmmm, water ', 3.60, 1.00, 2);
 INSERT INTO products(name, description, price, weight, category_id) VALUES('Domestos', 'Kill old bacteria and others!', 20.90, 1.00, 3);
+
+INSERT INTO statuses(name) VALUES('NIEZATIWERDZONE');
+INSERT INTO statuses(name) VALUES('ZATIWERDZONE');
+INSERT INTO statuses(name) VALUES('ANULOWANE');
+INSERT INTO statuses(name) VALUES('ZREALIZOWANE');
+
+INSERT INTO orders(state, approval_date, username, email, product_id) VALUES(1, "2021-10-31", 'amelinium23', '230025@edu.p.lodz.pl', 1);
+INSERT INTO orders(state, approval_date, username, email, product_id) VALUES(1, "2021-10-31", 'piot2000', 'email@gmail.com', 2);
+INSERT INTO orders(state, approval_date, username, email, product_id) VALUES(3, "2021-10-31", 'vertix', 'email.pass@wp.com', 6);
